a52cdeb4d7467cb1c6468f50b19af813e79e1d00
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-03-13 19:52:46 +0100

    Update file.asm

--- file.asm
+++ file.asm
@@ -1,121 +1,171 @@
 global _start
-    
+
+section .bss
+cNames resb 144
+input resb 1
+
 section .data
-configMessageOne db "## Configuration ##", 0x0a, "Entrez le nom du candidat:", 0x0a
-lenConfigMessage equ $ - configMessageOne
-adminMessage db "## Admin Mode ##:", 0x0a, "1 - Voter", 0x0a, "2 - Etat", 0x0a, "3 - Mettre à 0", 0x0a, "4 - Finir", 0x0a, "Choisisez une option :", 0x0a
-lenAdminMessage equ $ - adminMessage
-choiceNumber : db 1
-candidateNameOne db 15
-candidateNameTwo db 15
-candidateNameThr db 15
-candidateNameFou db 15
-candidateNameFiv db 15
-candidateNameSix db 15
-candidateNameSev db 15
-candidateNameEig db 15
-candidateVoteOne db 15
-candidateVoteTwo db 15
-candidateVoteThr db 15
-candidateVoteFou db 15
-candidateVoteFiv db 15
-candidateVoteSix db 15
-candidateVoteSev db 15
-candidateVoteEig db 15
-choice : db 15
+clearSequence db 27, "[H", 27, "[J"
+bootMsg db "Entrez le nom du candidat N - "
+adminMsg db "## Admin mode ##", 10, "0 - Votes à 0", 10, "1 - Afficher résultats", 10, "2 - Arreter la machine", 10, "3 - Reprendre le vote", 0x0a, "Entrez un choix:", 0x0a
+showVotesMsg db "## Resultats : ##", 10
+voteMsg db " - "
+cVotes db 0, 0, 0, 0, 0, 0, 0, 0, 0
+counter db 1, ":", 0x0a
 
 section .text
-    _start:
-        call printConfig
-        mov rax, choice
-        mov [candidateNameOne], rax 
+_start:
+    jmp bootMode
 
-        call printConfig
-        mov rax, choice
-        mov [candidateNameTwo], rax 
+clear:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, clearSequence
+    mov rdx, 6
+    syscall
 
-        call printConfig
-        mov rax, choice
-        mov [candidateNameThr], rax 
+    ret
 
-        call printConfig
-        mov rax, choice
-        mov [candidateNameFou], rax
+bootMode:
+    call clear
 
-        call printConfig
-        mov rax, choice
-        mov [candidateNameFiv], rax 
+    movzx r9, byte [counter]
+    add [counter], byte '0'
+    cmp r9, 9
+    jg adminMode
 
-        call printConfig
-        mov rax, choice
-        mov [candidateNameSix], rax
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, bootMsg
+    mov rdx, 30
+    syscall
 
-        call printConfig
-        mov rax, choice
-        mov [candidateNameSev], rax 
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, counter
+    mov rdx, 3
+    syscall
 
-        call printConfig
-        mov rax, choice
-        mov [candidateNameEig], rax
+    sub [counter], byte '0'
 
-        jmp admin
+    movzx r9, byte [counter]
+    dec r9
+    shl r9, 4
 
-    printConfig:
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, configMessageOne
-        mov rdx, lenConfigMessage
-        syscall
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, cNames
+    add rsi, r9
+    mov rdx, 17
+    syscall
 
-        mov rax, 0
-        mov rdi, 0
-        mov rsi, choice
-        mov rdx, 15
-        syscall
-        
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, choice
-        mov rdx, 15
-        syscall
-        ret
+    inc byte [counter]
+    jmp bootMode
 
-    admin:
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, adminMessage
-        mov rdx, lenAdminMessage
-        syscall
+adminMode:
+    call clear
 
-        mov rax, 0
-        mov rdi, 0
-        mov rsi, choiceNumber
-        mov rdx, 1
-        syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, adminMsg
+    mov rdx, 118
+    syscall
 
-        mov rax, qword [choiceNumber]
-        sub al, '0'
+    call getInput
 
-        cmp al, 2
-        je adminCase2
+    mov rax, [input]
+    cmp rax, 48
+    je resetVotes
 
-        mov rsi, rax
-        mov rdi, 1
-        mov rdx, 1
-        mov rax, 1
-        syscall
+    cmp rax, 49
+    je showVotes
 
-        mov rax, 60
-        mov rdi, 0
-        syscall
+    cmp rax, 50
+    je endVote
 
-    adminCase2:
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, candidateNameOne
-        mov rdx, 15
-        syscall
+    cmp rax, 51
+    je voteMode
 
-        mov rax, 60
-        mov rdi, 0
-        syscall
+    jmp adminMode
+
+voteMode:
+    call printVoteMsg
+
+    call getInput
+    jmp voteMode
+
+printVoteMsg:
+    cmp r9, 9
+    jge end_func
+
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 1
+    mov rsi, r9
+    add rsi, 48
+    syscall
+
+    mov rsi, voteMsg
+    mov rdx, 3
+    syscall
+
+    mov rsi, cNames
+    mov rdx, r9
+    imul rdx, 15
+    movzx rsi, byte [rsi+rdx]
+    mov rdx, 15
+
+    inc r9
+end_func:
+    ret
+
+resetVotes:
+    jmp adminMode
+
+showVotes:
+    mov byte [counter], 1
+
+    call clear
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, showVotesMsg
+    mov rdx, 18
+    syscall
+
+    jmp showVotesLoop
+
+showVotesLoop:
+    movzx r9, byte [counter]
+    cmp r9, 9
+    jg _sys_exit
+
+    add [counter], byte '0'
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, counter
+    mov rdx, 3
+    syscall
+
+    sub [counter], byte '0'
+
+    mov rax, 1
+    mov rdi, 1
+
+    inc byte [counter]
+    jmp showVotesLoop
+
+endVote:
+    jmp _sys_exit
+
+getInput:
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, input
+    mov rdx, 1
+    syscall
+
+    ret
+_sys_exit:
+    mov rax, 60
+    xor rdi, rdi
+    syscall
