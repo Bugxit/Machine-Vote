ef8d00ac2437991b4fabaa34ba43a73e42e20c44
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-03-14 10:41:35 +0100

    Update file.asm

--- file.asm
+++ file.asm
@@ -4,63 +4,46 @@ section .bss
 cNames resb 144
 input resb 1
 
+
 section .data
+sFile db 'candidats.txt', 0
 clearSequence db 27, "[H", 27, "[J"
-bootMsg db "Entrez le nom du candidat N - "
 adminMsg db "## Admin mode ##", 10, "0 - Votes à 0", 10, "1 - Afficher résultats", 10, "2 - Arreter la machine", 10, "3 - Reprendre le vote", 0x0a, "Entrez un choix:", 0x0a
 showVotesMsg db "## Resultats : ##", 10
+waitShowVotesMsg db "Entrez une touche :"
+showVotesMsg2 db ' : '
 voteMsg db " - "
+voteMsg2 db "Entrez un choix :", 0x0a
 cVotes db 0, 0, 0, 0, 0, 0, 0, 0, 0
-counter db 1, ":", 0x0a
+counter db 1
+
+nl db 0x0a
 
 section .text
 _start:
     jmp bootMode
 
-clear:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, clearSequence
-    mov rdx, 6
-    syscall
-
-    ret
-
 bootMode:
-    call clear
+    jmp getCandidates
 
-    movzx r9, byte [counter]
-    add [counter], byte '0'
-    cmp r9, 9
-    jg adminMode
-
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, bootMsg
-    mov rdx, 30
+getCandidates:
+    mov rax, 2
+    mov rdi, sFile
+    mov rsi, 0
     syscall
-
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, counter
-    mov rdx, 3
-    syscall
-
-    sub [counter], byte '0'
-
-    movzx r9, byte [counter]
-    dec r9
-    shl r9, 4
+    mov rbp, rax
 
     mov rax, 0
-    mov rdi, 0
+    mov rdi, rbp
     mov rsi, cNames
-    add rsi, r9
-    mov rdx, 17
+    mov rdx, 144
     syscall
 
-    inc byte [counter]
-    jmp bootMode
+    mov rax, 3
+    mov rdi, rbp
+    syscall
+
+    jmp adminMode
 
 adminMode:
     call clear
@@ -89,37 +72,75 @@ adminMode:
     jmp adminMode
 
 voteMode:
+    call clear
+    mov byte [counter], 1
     call printVoteMsg
-
-    call getInput
-    jmp voteMode
-
-printVoteMsg:
-    cmp r9, 9
-    jge end_func
+    mov byte [counter], 1
 
     mov rax, 1
     mov rdi, 1
-    mov rdx, 1
-    mov rsi, r9
-    add rsi, 48
+    mov rsi, voteMsg2
+    mov rdx, 18
     syscall
 
+    call getInput
+    cmp [input], byte 109
+    je adminMode
+
+    cmp [input], byte 48
+    je voteMode
+
+    mov r9, [input]
+    sub r9, 49
+    shl r9, 4
+    mov rax, cVotes
+    add rax, r9
+    mov byte [rax], 1
+
+    jmp voteMode    
+
+printVoteMsg:
+    movzx r9, byte [counter]
+    cmp r9, 9
+    jg end_func
+
+    add byte [counter], '0'
+    mov rsi, counter
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 1
+    syscall
+    sub byte [counter], '0'
+    
+    mov rax, 1
+    mov rdi, 1
     mov rsi, voteMsg
     mov rdx, 3
     syscall
 
     mov rsi, cNames
-    mov rdx, r9
-    imul rdx, 15
-    movzx rsi, byte [rsi+rdx]
-    mov rdx, 15
+    shl r9, 4
+    add rsi, r9
+    sub rsi, 16
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 16
+    syscall
 
-    inc r9
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, nl
+    mov rdx, 1
+    syscall
+
+    inc byte [counter]
+    jmp printVoteMsg
+
 end_func:
     ret
 
 resetVotes:
+    shl byte [cVotes+9], 9
     jmp adminMode
 
 showVotes:
@@ -137,23 +158,44 @@ showVotes:
 showVotesLoop:
     movzx r9, byte [counter]
     cmp r9, 9
-    jg _sys_exit
+    jg waitShowVotes
 
-    add [counter], byte '0'
+    mov rsi, cNames
+    shl r9, 4
+    add rsi, r9
+    sub rsi, 16
     mov rax, 1
     mov rdi, 1
-    mov rsi, counter
-    mov rdx, 3
+    mov rdx, 16
     syscall
 
-    sub [counter], byte '0'
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, showVotesMsg2
+    mov rdx, 3
+    syscall 
+
+    ;ICI AFFICHER VOTE POUR LES CANDIDATS
 
     mov rax, 1
     mov rdi, 1
+    mov rsi, nl
+    mov rdx, 1
+    syscall
 
     inc byte [counter]
     jmp showVotesLoop
 
+waitShowVotes:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, waitShowVotesMsg
+    mov rdx, 19
+    syscall
+
+    call getInput
+
+    jmp _sys_exit
 endVote:
     jmp _sys_exit
 
@@ -165,6 +207,15 @@ getInput:
     syscall
 
     ret
+clear:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, clearSequence
+    mov rdx, 6
+    syscall
+
+    ret
+
 _sys_exit:
     mov rax, 60
     xor rdi, rdi
